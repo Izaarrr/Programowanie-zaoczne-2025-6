ddcb77d7043d13cf3e6b26f8ba0e6cc6547af810
filipSlesicki <116304859+filipSlesicki@users.noreply.github.com>
2025-12-13 16:01:18 +0100

    Click to move

---
+++ Assets/DetectMouse.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock.LowLevel;
+
+public class DetectMouse : MonoBehaviour
+{
+    private Vector2 mousePosition;
+    [SerializeField] float rayRange = 5;
+    [SerializeField] LayerMask layer;
+    private bool clicked;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, layer))
+        {
+            if (clicked)
+            {
+                transform.position = hit.point;
+                transform.rotation = Quaternion.LookRotation(hit.normal);
+            }
+        }
+        clicked = false;
+    }
+
+    void OnMousePosition(InputValue value)
+    {
+        mousePosition = value.Get<Vector2>();
+    }
+
+    void OnAttack()
+    {
+        clicked = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        return;
+        Gizmos.color = Color.yellow;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, rayRange, layer))
+        {
+            Gizmos.DrawSphere(hit.point, 0.1f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(hit.point, hit.normal);
+        }
+        Gizmos.DrawRay(transform.position, transform.forward * rayRange);
+    }
+}
